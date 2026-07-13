a85bb99b130f7cfeb81f4edd7f7d10f59c22362f
marshal-ace <explorergautham@gmail.com>
2026-07-13 10:45:31 +0530

    LC 344 Completed

---
+++ Strings/strings-easy/344.py
@@ -0,0 +1,14 @@
+# 344. Reverse String
+# Write a function that reverses a string. The input string is given as an array of characters s.
+# You must do this by modifying the input array in-place with O(1) extra memory.
+
+s= ["h","e","l","l","o"]
+def reverse(s):
+    left=0
+    right=len(s)-1
+    while left<right:
+        s[left],s[right]=s[right],s[left]
+        left+=1
+        right-=1
+    return s
+print(reverse(s))
